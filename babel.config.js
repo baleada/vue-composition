de6aab5ca8c728a30d3122576c6bf711bf20372d
AlexVipond <alex.r.vipond@gmail.com>
2019-09-20 02:10:03 -0600

    fix gitignore to ignore babelified stuff

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 const presets = [
-  [
-    '@babel/preset-env',
-    {
-      targets: '> 0.5%, not dead',
-      // useBuiltIns: "usage",
-      // corejs: 3
-    },
-  ],
-],
+        [
+          '@babel/preset-env',
+          {
+            targets: '> 0.5%, not dead',
+            // useBuiltIns: "usage",
+            // corejs: 3
+          },
+        ],
+      ],
       plugins = [
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-proposal-private-methods'
