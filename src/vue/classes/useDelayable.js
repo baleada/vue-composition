1f9222e3f20c921afe7ce5f0a3ab40e71d38c005
AlexVipond <alex.r.vipond@gmail.com>
2019-12-19 14:05:34 -0600

    Update based on new metadata

--- src/vue/classes/useDelayable.js
+++ src/vue/classes/useDelayable.js
@@ -1,18 +1,9 @@
-import { ref, onMounted, onBeforeUnmount } from '@vue/composition-api'
-import { resolveRef, resolveOptionsRefs } from '../util'
+import { ref, onBeforeUnmount } from '@vue/composition-api'
 import { Delayable } from '@baleada/logic'
 
 export default function useDelayable (state, options) {
-  const reactiveInstance = ref({})
-  onMounted(() => {
-    state = resolveRef(state)
-    options = resolveOptionsRefs(options)
-    const instance = new Delayable(state, options)
-          // provisions = toProvisions(instance)
-
-    reactiveInstance.value = instance
-    // assignProvisions(reactiveInstance, provisions)
-  })
+  const instance = new Delayable(state, options),
+        reactiveInstance = ref(instance)
   onBeforeUnmount(() => reactiveInstance.value.stop())
   return reactiveInstance
 }
--- src/vue/classes/useListenable.js
+++ src/vue/classes/useListenable.js
@@ -1,18 +1,9 @@
-import { ref, onMounted, onBeforeUnmount } from '@vue/composition-api'
-import { resolveRef, resolveOptionsRefs } from '../util'
+import { ref, onBeforeUnmount } from '@vue/composition-api'
 import { Listenable } from '@baleada/logic'
 
 export default function useListenable (state, options) {
-  const reactiveInstance = ref({})
-  onMounted(() => {
-    state = resolveRef(state)
-    options = resolveOptionsRefs(options)
-    const instance = new Listenable(state, options)
-          // provisions = toProvisions(instance)
-
-    reactiveInstance.value = instance
-    // assignProvisions(reactiveInstance, provisions)
-  })
+  const instance = new Listenable(state, options),
+        reactiveInstance = ref(instance)
   onBeforeUnmount(() => reactiveInstance.value.stop())
   return reactiveInstance
 }
