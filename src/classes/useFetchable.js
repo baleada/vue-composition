064e12afef562149a3152d8b1969e7d0b3be83b0
AlexVipond <alex.r.vipond@gmail.com>
2020-03-23 15:05:29 -0600

    Don't track generated files

--- src/classes/useFetchable.js
+++
@@ -1,8 +0,0 @@
-import { ref } from '@vue/composition-api'
-import { Fetchable } from '@baleada/logic'
-
-export default function useFetchable (state, options) {
-const instance = new Fetchable(state, options),
-      reactiveInstance = ref(instance)
-return reactiveInstance
-}
--- src/classes/useRecognizeable.js
+++
@@ -1,8 +0,0 @@
-import { ref } from '@vue/composition-api'
-import { Recognizeable } from '@baleada/logic'
-
-export default function useRecognizeable (state, options) {
-const instance = new Recognizeable(state, options),
-      reactiveInstance = ref(instance)
-return reactiveInstance
-}
