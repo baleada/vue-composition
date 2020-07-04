99a88aa55b7e49e50a39dde2ba1d680e68ca9c68
AlexVipond <alex.r.vipond@gmail.com>
2020-07-03 18:37:03 -0600

    add module to package json

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,39 @@
 import babel from 'rollup-plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
+const metadata = require('@baleada/logic/metadata')
 
-export default {
-  external: [
-    '@vue/composition-api',
-    '@baleada/logic/factories',
-    '@baleada/logic',
-    'vue',
-  ],
-  input: [
-    'src/index.js',
-  ],
-  output: {
-    dir: 'lib',
-    format: 'esm',
+const plugins = [
+        babel({
+          exclude: 'node_modules',
+        }),
+        resolve(),
+      ]
+
+export default [
+  {
+    external: [
+      '@baleada/logic',
+      'vue',
+    ],
+    input: [
+      'src/index.js',
+    ],
+    output: {
+      dir: 'lib',
+      format: 'esm',
+    },
+    plugins,
   },
-  plugins: [
-    babel({
-      exclude: 'node_modules',
-    }),
-    resolve(),
-  ]
-}
+  // ...metadata.classes.map(tool => ({
+  //   external: '@baleada/logic',
+  //   input: `src/classes/use${tool.name}.js`,
+  //   output: { file: `classes/use${tool.name}.esm.js`, format: 'esm' },
+  //   plugins,
+  // })),
+  // ...metadata.factories.map(tool => ({
+  //   external: '@baleada/logic',
+  //   input: `src/factories/use${tool.name}.js`,
+  //   output: { file: `factories/use${tool.name}.esm.js`, format: 'esm' },
+  //   plugins,
+  // }))
+]
